944de0815d58c68881691799378b9ee4822f8e79
Steve Busi <steve.busi@dudesolutions.com>
2023-03-13 22:49:07 -0400

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# Simple Chat Bot Example
+
+## Usage
+
+Create virtual environment and restore dependencies  
+`export OPENAI_API_KEY=<YourAPIKey>`
+`python app.py`  
+
+`load_document()` loads files from disk and indexes them.  
+`generate_response()` calls the openai Completion API endpoint
+
+## Todo
+
+* This appears to use GPT2 for indexing. Need to verify.
+
+## Source
+* https://hackthedeveloper.com/openai-chatgpt-api-python-guide/  
+* https://bootcamp.uxdesign.cc/a-step-by-step-guide-to-building-a-chatbot-based-on-your-own-documents-with-gpt-2d550534eea5
---
+++ app.py
@@ -0,0 +1,36 @@
+import os
+from os.path import exists
+from timeit import default_timer
+
+import openai
+from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader
+
+openai.api_key = os.environ.get('OPENAI_API_KEY')
+
+def load_document():
+    start = default_timer()
+    if not exists('index/index.json'):
+        documents = SimpleDirectoryReader('data').load_data()
+        index = GPTSimpleVectorIndex(documents)
+        index.save_to_disk('index/index.json')
+    index = GPTSimpleVectorIndex.load_from_disk('index/index.json')
+    print(f'Document loaded and indexed in {default_timer() - start} seconds')
+    return index
+
+
+def generate_response(prompt):
+    response = openai.Completion.create(
+        engine='text-davinci-002',
+        prompt=prompt,
+        max_tokens=60,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    return response.choices[0].text.strip()
+
+index = load_document()
+prompt = input('Ask me a question: ')
+# response = generate_response(prompt)
+response = index.query(prompt)
+print(response)
